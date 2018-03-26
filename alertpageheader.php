812d5aff1b68293dd469a908ed5378d4f779c275
T.MAHARANI <maharani1118@gmail.com>
2018-03-26 14:33:36 +0530

    Create alertpageheader.php

---
+++ alertpageheader.php
@@ -0,0 +1,44 @@
+<style type="text/css">
+hr.style5 {
+	background-color:#FF0000;
+	border:groove;
+	border-color:#0000CC;
+	
+}
+
+	hr.style2 {
+	border-top: 3px double #8c8b8b;
+	border-color:#FF0066;
+}
+.flash {
+   animation-name: flash;
+    animation-duration: 0.2s;
+    animation-timing-function: linear;
+    animation-iteration-count: infinite;
+    animation-direction: alternate;
+    animation-play-state: running;
+}
+@keyframes flash {
+    from {color:#FF0066;}
+    to {color: black;}
+
+
+</style>
+<body>
+ <div>
+   <table align="center" height="25%" width="90%" border="0" cellpadding="0" cellspacing="0" >
+     <tr>
+      <td align="center" height="20%" width="15%"><img src="images/adminsignuplogo.png" height="50%" width="60%" /> </td>
+     <!-- <td align="center" height="25%" width="35%">
+	  <marquee behavior="alt" direction="down"><center>
+	  <h2 class="flash"><font face="Arial Rounded MT" size="+2"> SCHOOL CONSTRUCTION TENDER </font></h2></center></marquee>
+      </td>-->
+	  <td align="center" height="25%" width="60%"><img src="images/correct1.PNG" height="80%" width="80%" /> </td>
+       <td align="center" height="25%" width="25"><img src="images/correct2.PNG" height="75%" width="100%" /> </td>
+     
+	  </tr>
+	 </table>
+  </div>
+  <hr class="style5"  />
+  
+</body>
